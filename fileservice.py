a848bb999fadfbf7ff40cba7c3bbaae80797f314
Squid Grill / Royce551 <31809541+Royce551@users.noreply.github.com>
2019-05-15 20:31:24 -0500

    Added support for creating configuration files

--- fileservice.py
+++ fileservice.py
@@ -10,4 +10,16 @@ def loadconfig():
     alttoken = config['Data']['alttoken']
     prefix = config['Data']['prefix']
     return Version, token, alttoken, prefix;
+def cserverconfig(sID, sCH, sMO):
+    config = configparser.ConfigParser()
+    config.add_section("Data")
+    config.add_section("Flags")
+    config['Data']['Version'] = '1'
+    config['Data']['Server ID'] = sID
+    config['Flags']['SuggestChannel'] = sCH
+    config['Data']['Moderators'] = sMO
+    with open(sID + '.sffs', 'w') as configfile:
+        config.write(configfile)
+
+cserverconfig('test', '123', '345')
 
